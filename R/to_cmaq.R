325ce67408bdb405322951ac6271252a6466d6ac
ibarraespinosa <zergioibarra@gmail.com>
2017-11-24 23:26:12 -0200

    to_cmaq

---
+++ R/to_cmaq.R
@@ -0,0 +1,19 @@
+#' Creating text files for runnign CMAQ
+#'
+#' @description Create an emission file from wrfinput
+#'
+#' @param x file
+#' @param ... ignored
+#' @note in development. Needs description for creating grid vein::make_grid
+#' and other information
+#'
+#' @export
+#'
+#'
+#' @examples \dontrun{
+#'}
+
+to_cmaq  <- function(x, ...){
+
+}
+
