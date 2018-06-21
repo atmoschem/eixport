c02eb36de994d151d2b3fe514500954e0e4b41c4
Schuch666 <underschuch@gmail.com>
2018-06-21 14:11:00 -0300

    test wrf_create fix

--- tests/testthat/test-wrf_create.R
+++ tests/testthat/test-wrf_create.R
@@ -1,19 +1,23 @@
 context("wrf_create")
+
 dir.create(file.path(tempdir(), "EMISS"))
 wrf_create(wrfinput_dir          = system.file("extdata", package = "eixport"),
             wrfchemi_dir         = file.path(tempdir(), "EMISS"),
             domains              = 2,
             frames_per_auxinput5 = 1,
-            auxinput5_interval_m = 60,
-            day_offset           = 1,
             verbose              = FALSE)
-f1 <- paste0(file.path(tempdir(), "EMISS"),
-             "/wrfchemi_d02_2011-08-02_00:00:00")
+
+f1 <- dir(path = file.path(tempdir(), "EMISS"), pattern = "wrfchemi_d02", full.names = T)
+
 f2 <- unzip(zipfile = paste0(system.file("extdata", package = "eixport"),
                              "/wrfchemi_d02_2011-08-02.zip"),
             exdir = file.path(tempdir()))
+
+# f2 <- "wrfchemi_d02_2011-08-02"
+
 nc1 <- ncdf4::nc_open(f1)
 nc2 <- ncdf4::nc_open(f2)
+
 test_that("wrf_create works", {
   expect_equal(nc1$nvars,
                nc2$nvars
