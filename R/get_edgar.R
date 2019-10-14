78df1bf1d36ba113f49a2e31d4e1a047a4ab822d
ibarraespinosa <sergio.ibarra@usp.br>
2019-10-14 16:19:52 -0300

    adding get_edgar, now depends on 3.5.0

---
+++ R/get_edgar.R
@@ -0,0 +1,150 @@
+#' Download datasets of EDGAR emissions
+#'
+#' @description The Emissions Database for Global Atmospheric Research (EDGAR) is a
+#' project from the Joint Research Centre. They provide provides global past and
+#' present day anthropogenic emissions of greenhouse gases and air
+#' pollutants by country and on spatial grid. \code{\link{get_edgar}} provide
+#' functions to download any of the EDGAR datasets.
+#'
+#'
+#' @param dataset Character; name of the datasets: "v432", "v432_AP", "v432_VOC_spec",
+#' "v4tox2", "htap_v2_2"
+#' @param pol Character; one of the pollutants shown on note.
+#' @param sector Character; one of the sectors shown on note.
+#' @param year Integer; on of the years shown on note.
+#' @param destpath Character: Path to create the directory for downloads datasets
+#' @param txt Logical; if TRUE, download data as .txt and untis t/year,
+#' if FALSE units are ug/m2/s
+#' @param copyright Logical; to show copyright information.
+#' @return Downloads data
+#' @note
+#'
+#' \tabular{lllll}{
+#'   dataset  \tab pollutant \tab            sector  \tab years\tab monthly \cr
+#'      -     \tab      -    \tab      -            \tab -\cr
+#'      v432  \tab    CH4, N2O, CO2_excl_short-cycle_org_C, CO2_org_short-cycle_C
+#'            \tab     AGS, CHE, ENE, ENF, FFF, IND, IRO, MNM, PRO, REF_TRF, SWD_INC,
+#'             SWD_LDF, TNR_Aviation_CDS, TNR_Aviation_CRS,
+#'             TNR_Aviation_LTO, TNR_Aviation_SPS, TNR_Other, TNR_Ship,
+#'             TOTALS, TRO, WWT \tab 1970-2012 \tab 2010\cr
+#'      -     \tab    -      \tab - \tab -  \cr
+#'   v432_AP  \tab   BC, CO, CO2, NH3, NMVOC, NOx, OC, PM10,
+#'   PM2.5, PM2.5_bio, PM2.5_fossil, SO2  \tab  AWB, CHE, ENE, FFF, FOO_PAP, IND, IRO, NFE, NMM, PRO,
+#'                RCO, REF_TRF, SWD_INC, TNR_Aviation_CDS, TNR_Aviation_CRS,
+#'                TNR_Aviation_LTO, TNR_Aviation_SPS, TNR_Other, TNR_Ship, TOTALS,
+#'                TRO \tab 1970-2012 \tab 2010\cr
+#'      -     \tab    -      \tab  -\tab  - \cr
+#'   v432_VOC_spec \tab   voc1, voc2, voc3, voc3, voc5, voc6, voc7, voc8, bvoc9, voc10.
+#'                       voc11, voc12, voc13, voc14, voc15, voc16, voc17, voc18, voc19, voc20,
+#'                       voc21, voc22, voc23, voc24, voc25, TOTALS
+#'                          \tab  ENE, IND, PPA, RCO, REF
+#'                          TNR_Aviation_CDS, TNR_Aviation_CRS,
+#'                TNR_Aviation_LTO, TNR_Aviation_SPS, TNR_Other, TNR_Ship, TOTALS,
+#'                TRO, TRF \tab 1970-2012 \tab 2010\cr
+#'      -     \tab    -      \tab  -\tab  - \cr
+#'      v4tox2  \tab    HG, HG_D, HG_G, HG_P \tab Iro,  cement, chlor, comb_power_ind,
+#'      comb_res_oth, glass, gold_A, gold_L, nfe_oth, shipping, totals, tro_roa,
+#'      waste \tab 1970-2008 \tab no \cr
+#'      -     \tab    -      \tab  -\tab  - \cr
+#'      htap_v2_2  \tab BC, CO, NH3, NMVOC, NOx, OCm, PM10, PM2.5, SO2 \tab
+#'      AIR, ENERGY, INDUSTRY, RESIDENTIAL, SHIPS TRANSPORT \tab 2008 and 2010 \tab 2008 and 2010\cr
+#' }
+#'
+#'
+#'
+#' @references
+#'
+#'- **v432**:  Muntean, M., Guizzardi, D., Schaaf, E., Crippa, M., Solazzo, E.,
+#'Olivier, J.G.J., Vignati, E. Fossil CO2 emissions of all world countries - 2018
+#'Report, EUR 29433 EN, Publications Office  of the European Union, Luxembourg,
+#'2018, ISBN 978-92-79-97240-9, doi:10.2760/30158, JRC113738.
+#'- **v432_AP**:Crippa, M., Guizzardi, D., Muntean, M., Schaaf, E., Dentener, F.,
+#'van Aardenne, J. A., Monni, S., Doering, U., Olivier, J. G. J., Pagliari, V.,
+#'and Janssens-Maenhout, G.: Gridded emissions of air pollutants for the period
+#'1970–2012 within EDGAR v4.3.2, Earth Syst. Sci. Data, 10, 1987–2013,
+#'https://doi.org/10.5194/essd-10-1987-2018, 2018.
+#'- **v432_VOC_spec**: Huang, G., Brook, R., Crippa, M., Janssens-Maenhout, G.,
+#'Schieberle, C., Dore, C., Guizzardi, D., Muntean, M., Schaaf, E., and Friedrich, R.:
+#'Speciation of anthropogenic emissions of non-methane volatile organic compounds: a
+#'global gridded data set for 1970–2012, Atmos. Chem. Phys., 17, 7683-7701,
+#'https://doi.org/10.5194/acp-17-7683-2017, 2017.
+#'- **v4tox2**: Muntean, M., Janssens-Maenhout, G., Song, S., Giang, A., Selin, N. E.,
+#'Zhong, H., ... & Schaaf, E. (2018). Evaluating EDGARv4. tox2 speciated mercury
+#'emissions ex-post scenarios and their impacts on modelled global and regional
+#'wet deposition patterns. Atmospheric Environment, 184, 56-68.
+#'- **htap_v2_2**: Janssens-Maenhout, G., Crippa, M., Guizzardi, D.,
+#'Dentener, F., Muntean, M., Pouliot, G., Keating, T., Zhang, Q., Kurokawa, J.,
+#'Wankmüller, R., Denier van der Gon, H., Kuenen, J. J. P., Klimont, Z.,
+#'Frost, G., Darras, S., Koffi, B., and Li, M.: HTAP_v2.2: a mosaic of
+#'regional and global emission grid maps for 2008 and 2010 to study
+#'hemispheric transport of air pollution, Atmos. Chem. Phys., 15,
+#'11411–11432, https://doi.org/10.5194/acp-15-11411-2015, 2015.
+#' @importFrom utils download.file
+#' @export
+#' @examples \dontrun{
+#' # Do not run
+#' get_edgar()
+#' }
+get_edgar <- function(dataset = "v432_AP",
+                      pol = "CO",
+                      sector = "TRO",
+                      year = 2012,
+                      destpath = tempdir(),
+                      txt = TRUE,
+                      copyright = TRUE){
+  if(copyright) message(
+    paste0(c(
+      " Copyright notice\n",
+      "------------------\n",
+      " (c) European Union, 1995-2019\n\n",
+      " Reuse is authorised, provided the source is acknowledged. The reuse policy \n",
+      " of the European Commission is implemented by a Decision of 12 December 2011, see\n",
+      " http://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=OJ:L:2011:330:0039:0042:EN:PDF\n\n"
+    ))
+  )
+
+
+  ed <- sysdata$edgar
+
+  # Check years
+  if(dataset == "htap_v2_2") {
+    if(!year %in% c(2008, 2010)) {
+      stop("When dataset is htap_v2_2, years can be 2008 or 2010 only")
+    }
+  } else {
+    if(!year %in% 1970:2012){
+      stop("For this datasets, years go from 1970 to 2012 only")
+    }
+  }
+
+  # links
+  df <- ed[ed$dataset %in% dataset &
+             ed$pol %in% pol &
+             ed$sector %in% sector, ]
+
+    links = unlist(lapply(1:length(df$URL), function(i) {
+    eval(parse(text = df$URL[i]))
+  }))
+  if(txt){
+    links <- gsub(pattern = ".0.1x0.1", replacement = "", x = links)
+  }
+  df$links <- links
+
+  # extension
+  txz <- if(txt){
+    ".zip"
+  } else {
+    ".0.1x0.1.zip"
+  }
+  txzz <- eval(parse(text = df$txz))
+
+  # paths
+  for (i in 1:length(links)){
+    utils::download.file(paste0(url = df$links[i]),
+                         destfile =  paste0(destpath, "/",
+                                            txzz[i]))
+    message(paste0(
+      "Files at", destpath, "/", txzz[i], "\n"))
+  }
+
+}
